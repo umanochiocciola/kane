0198612f41c553f643248577d0a4c8f6a71b3467
umanochiocciola <59803009+umanochiocciola@users.noreply.github.com>
2020-11-06 21:41:01 +0100

    Update terminal.py

--- MainKane/terminal.py
+++ MainKane/terminal.py
@@ -6,8 +6,14 @@ import os
 import glob
 import pickle as pk
 import traceback
+import urllib.request
+from datetime import date
 
-version = '2.0.8'
+today = date.today()
+DATE = today.strftime("%d/%m/%Y")
+LAST_CHACKED = DATE
+
+version = '2.0.9.3'
 
 try:
     import ctypes
@@ -30,27 +36,57 @@ def cd_punto(tin):
 
 directory = str(pathlib.Path().absolute())
 passw = ''
-elp = "Kane 2.0 terminal manual: \n list of internal commands\n   type wiki to get more information. \n\n start:  open a program \n !quit: quit terminal \n web open a URL in browser\n !helpKane OR !help:  get this list ;)\n cd: jump to a directoryectory\n directory OR ls: list files in the folder\nupgrade: Upgrade kane version\n man <command>: read further informations about a command\n While giving a path, you can use * to referr to the current directoryectory\n makedirectory <directory>: create a directoryectory at the specified path.\n py <python command>: execute python line\n home: jump to home user folder"
+elp = "Kane 2.0 terminal manual: \n list of internal commands\n   type wiki to get more information. \n\n start:  open a program \n quit: quit terminal \n web open a URL in browser\n !helpKane OR help:  get this list ;)\n cd: jump to a directoryectory\n directory OR ls: list files in the folder\nupgrade: Upgrade kane version\n man <command>: read further informations about a command\n While giving a path, you can use * to referr to the current directoryectory\n makedirectory <directory>: create a directoryectory at the specified path.\n py <python command>: execute python line\n home: jump to home user folder"
 
 username = input('$ Username: ')
 if os.path.exists(f'{directory}/users/{username}'):
     with open(f'{directory}/users/{username}/UserData.dat', 'rb') as f:
-        passw = pk.load(f)[0]
+        passw, LAST_CHACKED = pk.load(f)
     if passw != '':
         while input(f'{username}\'s password: ') != passw:
             print('wrong password.\n')
         print('Welcome')
 else:
     print('creating user...')
-    os.makedirectorys(f'{directory}/users/{username}')
+    os.makedirs(f'{directory}/users/{username}')
     with open(f'{directory}/users/{username}/UserData.dat', 'wb') as f:
-        pk.dump([passw], f, protocol=2)
+        pk.dump([passw, LAST_CHACKED], f, protocol=2)
+
+if LAST_CHACKED != 'DATE':
+    print('+==================+')
+    print('|Daily update check|')
+    print('+==================+\n')
+    
+    fp = urllib.request.urlopen("https://raw.githubusercontent.com/umanochiocciola/kane/main/version.txt")
+    mybytes = fp.read()
+    fp.close()
+    latest = str(mybytes.decode("utf8"))
+    print(f'Your version: {version}')
+    print(f'Latest version avaiable: {latest}\n')
+    
+    print('If you want to update, use the   upgrade   command')
 
 InternalCommands = {
-    'test': 'it works!',
-    '!helpKane': elp,
-    'help': elp
+    'test': "print('it works!')",
+    '!helpKane': "print(elp)",
+    'help': "print(elp)",
+    'scream': "scream()",
+    'gimmeh comics': "import antigravity",
+    'cake': "print('You really need me to tell you that EVERY cake is a lie?')",
+    "lemme see": "dir()",
+    "spin!": "spin()"
 }
+
+'''geeky stuff'''
+def scream():
+    try:
+        print('\aWhoo')
+    except:
+        print("sorry, apparently this doesn't work on your main OS")
+
+def spin():
+    print('weeeeee')
+'''bicos ies'''
 
 user = username
 
@@ -85,6 +121,14 @@ while True:
         except:
             1+1
     
+    if 'read ' in command:
+        try:
+            if command[0] + command[1] + command[2] + command[3] == 'read':
+                with open(f"{directory}/{command.replace('read ')}") as f:
+                    print(f.read())
+        except:
+            1+1
+    
     elif 'start ' in command:
         try:
             os.startfile(directory + '/' + command.replace('start ', '').replace('*', directory))
@@ -98,16 +142,16 @@ while True:
         print(f'Changing password for {user}')
         passw = input('$:>')
         with open(f'{directory}/users/{username}/UserData.dat', 'wb') as f:
-            pk.dump([passw], f, protocol=2)
+            pk.dump([passw, LAST_CHACKED], f, protocol=2)
             print(f'saved succesfully!\n')
         
             
-    elif 'makedirectory ' in command:
-        dar = command.replace('makedirectory ', '')
+    elif 'makedir' in command:
+        dar = command.replace('makedir ', '')
         if not '/' in dar:
             dar = f'{directory}/{dar}'
         if not os.path.exists(dar):
-            os.makedirectorys(dar)
+            os.makedirs(dar)
             print(f'{dar} succesfully created!')
     
     elif command == 'home':
@@ -152,16 +196,16 @@ while True:
         user = input('$> ')
         if not os.path.exists(f'{str(pathlib.Path().absolute())}/users/{user}'):
             passw = '' 
-            os.makedirectorys(f'{str(pathlib.Path().absolute())}/users/{user}')
+            os.makedirs(f'{str(pathlib.Path().absolute())}/users/{user}')
             with open(f'{str(pathlib.Path().absolute())}/users/{user}/UserData.dat', 'wb') as f:
-                pk.dump([user, passw], f, protocol=2)
+                pk.dump([passw, LAST_CHACKED], f, protocol=2)
                 print(f'{user} folder created succesfully!!')
         else:
             with open(f'{str(pathlib.Path().absolute())}/users/{user}/UserData.dat', 'wb') as f:
-                user, passw = pk.load(f)
+                passw, LAST_CHACKED = pk.load(f)
         try:
             with open('UserData.dat', 'wb') as f:
-                pk.dump([user, passw], f, protocol=2)
+                pk.dump([passw, LAST_CHACKED], f, protocol=2)
                 print(f'saved succesfully!')
             if passw != '':
                 if input(f'{user}\'s password: ') == passw:
@@ -196,5 +240,5 @@ while True:
         if ab == 'fuc':
             subprocess.call(f'cd {directory}&{command}', shell=True)
         else:
-            print(InternalCommands.get(command, 'Uknown Internal, directoryect or external command.'))
+            exec(InternalCommands.get(command, "print('Uknown Internal, directoryect or external command.')"))
     print('   ')
